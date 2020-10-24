14c0ea669457f1b999926c8dfd96e54d9779fd65
Manfred Stock <m-osm@nfred.ch>
2020-10-24 22:32:55 +0200

    Add basic Content-Security-Policy

---
+++ js/slides.js
@@ -0,0 +1,10 @@
+(function strict() {
+    'use strict';
+    Reveal.initialize({
+        hash: true,
+        respondToHashChanges: true,
+        loop: true,
+        controlsTutorial: false,
+        autoSlide: 5000
+    });
+})();
